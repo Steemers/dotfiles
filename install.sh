98f1ee3c0464df79d204136517df46b375ea779b
Steemers <112313769+Steemers@users.noreply.github.com>
2022-11-28 14:06:55 +0100

    Add mv dotfiles zsh

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ python3 -m pip install ciphey --upgrade
 mv ~/dotfiles/resources/fonts/*.ttf ~/.local/share/font
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+mv ~/dotfiles.p10k.zsh ~/
+mv ~/dotfiles.zshrc ~/
