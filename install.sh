90d9bbdbd97ab501ef8a88ae68e8f4fd3ac078a7
Steemers <112313769+Steemers@users.noreply.github.com>
2022-11-28 13:54:10 +0100

    Add p10k install

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "test"
 sudo apt-get update
 sudo apt-get upgrade -y
 
 python3 -m pip install ciphey --upgrade
+
+mv ~/dotfiles/resources/fonts/*.ttf ~/.local/share/font
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
