605591ba4152c1810159ef9c225525c64d8fc620
Steemers <112313769+Steemers@users.noreply.github.com>
2022-12-01 13:06:49 +0100

    remove ciphey installation

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-python3 -m pip install ciphey --upgrade
-
 mv ~/dotfiles/resources/fonts/*.ttf ~/.local/share/font
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
