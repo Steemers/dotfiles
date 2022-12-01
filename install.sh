71927ed591ff6362e4d6882c909d33e55c898ad0
Pim Steemers <steemers15@gmail.com>
2022-12-01 08:05:18 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 mkdir ~/.local/share/fonts
-mv ~/dotfiles/resources/fonts/*.ttf ~/.local/share/fonts
+cp ~/dotfiles/resources/fonts/*.ttf ~/.local/share/fonts
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 cp ~/dotfiles/.p10k.zsh ~/
