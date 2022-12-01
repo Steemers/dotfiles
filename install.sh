55e3d595c9e4b65cc05f895895457c2d25fec8be
Pim Steemers <steemers15@gmail.com>
2022-12-01 08:00:27 -0500

    add gitconfigure file and update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ mkdir ~/.local/share/fonts
 mv ~/dotfiles/resources/fonts/*.ttf ~/.local/share/fonts
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-mv ~/dotfiles/.p10k.zsh ~/
-mv ~/dotfiles/.zshrc ~/
+cp ~/dotfiles/.p10k.zsh ~/
+cp ~/dotfiles/.zshrc ~/
