a133f706fc90bb30834823fd47d0b13a68db4669
Steemers <112313769+Steemers@users.noreply.github.com>
2022-12-01 13:24:22 +0100

    update install.sh remove typo's

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-mv ~/dotfiles/resources/fonts/*.ttf ~/.local/share/font
+mkdir ~/.local/share/fonts
+mv ~/dotfiles/resources/fonts/*.ttf ~/.local/share/fonts
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 mv ~/dotfiles/.p10k.zsh ~/
